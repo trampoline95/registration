2234a3449cddd14bb72f18befc11d09e69dfba43
trampoline95 <mohanty.ansman@gmail.com>
2020-04-14 23:28:16 +0530

    fix

--- pages/Dialog.js
+++ pages/Dialog.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,103 +6,48 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { useState } from 'react';
 
-export default function FormDialog(props) {
-    const [open, setOpen] = useState(false);
-    const [User, setUser] = useState("");
-    const [Pass, setPass] = useState("")
-    const [Phn, setPhn] = useState("")
-    const [Email, setEmail] = useState("")
-  
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      props.onEdit()
-    };
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
 
-    const handleName = (e) => {
-      setUser(e.target.value);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handlePhoneNumber = (e) => {
-      setPhn(e.target.value);
-    };
-
-    const handleEmail = (e) => {
-      setEmail(e.target.value);
-    };
-
-    const handlePassword =(e) => {
-      setPass(e.target.value);
-    };
-
-    const updateProfile = () =>{
-        alert("Profile Updated Successfully")
-        localStorage.setItem('regUser',User)
-        localStorage.setItem('regPass',Pass)
-        localStorage.setItem('regPhn',Phn)
-        localStorage.setItem('regEmail',Email)
-        props.onEdit();
-    }
-
-    console.log(props)
-  
-    return (
-      
-        <Dialog open={props.isopen} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Edit Profile</DialogTitle>
-          <DialogContent>
-            
-            <TextField
-              autoFocus
-              margin="dense"
-              id="firstName"
-              label="First Name"
-              type="text"
-              fullWidth
-              onChange = {handleName}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="phoneNumber"
-              label="Phone Number"
-              type="text"
-              fullWidth
-              onChange = {handlePhoneNumber}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="email"
-              label="Email Address"
-              type="text"
-              fullWidth
-              onChange = {handleEmail}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="password"
-              label="Password"
-              type="password"
-              fullWidth
-              onChange = {handlePassword}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={updateProfile} color="primary">
-              Submit
-            </Button>
-          </DialogActions>
-        </Dialog>
-      
-    );
-  }
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
